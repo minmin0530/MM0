20787848c0cd4b1f6911fd5f5e07da36a658b7cf
izumi_yoshiki <izumiyoshiki@icloud.com>
2018-06-16 00:05:02 +0900

    first commit

---
+++ MM0/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  MM0
+//
+//  Created by IzumiYoshiki on 2018/06/16.
+//  Copyright © 2018年 IzumiYoshiki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
